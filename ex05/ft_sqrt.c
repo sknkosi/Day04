d414421b85ef97c229958a6e61ba33cb81a48490
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-21 15:22:55 +0200

    Finished the updates

---
+++ ex05/ft_sqrt.c
@@ -0,0 +1,12 @@
+/*The function returns the square root of a number (if it exists), or 0 if the square root is an irrational number. */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
