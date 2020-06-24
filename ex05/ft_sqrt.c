0439db4a805714d1702969de104195fad4476c36
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-24 10:33:08 +0200

    Finished Ex05

--- ex05/ft_sqrt.c
+++ ex05/ft_sqrt.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sknkosi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/24 10:32:34 by sknkosi           #+#    #+#             */
+/*   Updated: 2020/06/24 10:32:40 by sknkosi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*The function returns the square root of a number (if it exists), or 0 if the square root is an irrational number. */
 
 int	ft_fibonacci(int index)
